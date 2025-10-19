690db222b66c81af01b158a78df1a95949af99e7
Nishad <nishadkanago@gmail.com>
2025-10-19 13:16:14 +0530

    temperature insert

--- internal/store/temprature.go
+++ internal/store/temprature.go
@@ -1,7 +1,10 @@
 package store
 
 import (
+	"context"
 	"iot/data_simulator/common"
+	"log"
+	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 )
@@ -11,5 +14,33 @@ type TemperatureStore struct {
 }
 
 func (s *TemperatureStore) InsertBatch(data []common.Metrics) error {
-	return nil
+	batch, err := (*s.ch).PrepareBatch(context.Background(), "INSERT INTO temperature (id, device_name, device_id, baseline_temperature, spike_probability, spike_magnitude, noise_level, updated_interval, drift_rate, current_temperature, is_spiking, last_spike_time, next_read_time, trend)")
+	if err != nil {
+		log.Printf("Error preparing batch: %v", err)
+		return err
+	}
+	for _, i := range data {
+		updateTime := time.Now().Add(i.UpdateInterval)
+
+		if err := batch.Append(
+			i.Id,
+			i.DeviceName,
+			i.DeviceId,
+			i.BaselineTemp,
+			i.SpikeProbability,
+			i.SpikeMagnitude,
+			i.NoiseLevel,
+			updateTime,
+			i.DriftRate,
+			i.CurrentTemp,
+			i.IsSpiking,
+			i.LastSpikeTime,
+			i.NextRead,
+			i.Trend,
+		); err != nil {
+			log.Printf("Error appending to batch: %v", err)
+			return err
+		}
+	}
+	return batch.Send()
 }
