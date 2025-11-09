63d55a05db463a909f9357903b73bef98e823f0e
Nishad <nishadkanago@gmail.com>
2025-11-09 22:19:27 +0530

    temperature static data UI

--- FrontEnd/iot-dashboard/src/pages/Static/Temperature.jsx
+++ FrontEnd/iot-dashboard/src/pages/Static/Temperature.jsx
@@ -1,9 +1,33 @@
-const Temperature = () => {
+import React from "react";
+import DataTable from "../../components/DataTable";
+
+function Temperature() {
+  const temperatureColumns = [
+    "device_id",
+    "loc",
+    "model",
+    "manufacturer",
+    "install_date",
+    "baseline_temperature",
+    "spike_probability",
+    "spike_magnitude",
+    "noise_level",
+    "current_temperature",
+    'drift_rate',
+    "is_spiking",
+    'trend',
+    "last_spike_time",
+    "updated_at",
+  ];
+
   return (
-    <div className="text-center text-lg font-semibold">
-      Static Data - Temperature Table Placeholder
+    <div className="container mt-4">
+      <DataTable
+        apiBaseUrl="http://localhost:8080/v1/temperature/statistics"
+        columns={temperatureColumns}
+      />
     </div>
   );
-};
+}
 
 export default Temperature;
